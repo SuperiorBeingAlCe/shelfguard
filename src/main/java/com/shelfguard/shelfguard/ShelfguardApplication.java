394ba43477675afa0ea0fbf15ab4d88d0de3f87f
Your Name <alperen.v.celebi44@gmail.com>
2026-02-02 21:42:03 +0300

    chore: project bootstrap with spring boot base setup

---
+++ src/main/java/com/shelfguard/shelfguard/ShelfguardApplication.java
@@ -0,0 +1,13 @@
+package com.shelfguard.shelfguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shelfguard/shelfguard/ShelfguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelfguard.shelfguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
